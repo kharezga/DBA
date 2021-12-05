d984c18cdd2b164150568cc649c2b4758ee137cf
Kacper <50089255+kharezga@users.noreply.github.com>
2021-12-05 15:25:36 +0100

    Keypoints extracion with Iris recognition

--- DriverBehaviorAnalyser/DriverAnalyser.py
+++ DriverBehaviorAnalyser/DriverAnalyser.py
@@ -10,6 +10,23 @@ def extractKeypoints(result):
     return face
 
 
+def extractKeypointsWithIris(result):
+    """
+    Extracts values of landmark of each coord (x,y,z) and assigns it to the flattened numpy array. If results don't
+    exists the array of zeros is assigned.
+
+    result: all landmarks of the deteced face
+    """
+
+    if result.multi_face_landmarks:
+        for face_detected in result.multi_face_landmarks:
+            face = np.array([[res.x, res.y, res.z] for res in face_detected.landmark]).flatten()
+    else:
+        face = np.zeros(1434)
+
+    return face
+
+
 def drawCustomLandmarks(image, results, holistic):
     mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
 
@@ -17,6 +34,28 @@ def drawCustomLandmarks(image, results, holistic):
                               mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
                               mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1,
                                                      circle_radius=1))
+
+
+def drawCustomLandmarksWithIris(image, results, mp_face_mesh):
+    mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
+    mp_drawing_styles = mp.solutions.drawing_styles
+
+    annotated_image = image.copy()
+    if results.multi_face_landmarks is not None:
+        for face_landmarks in results.multi_face_landmarks:
+            mp_drawing.draw_landmarks(image=annotated_image,
+                                      landmark_list=face_landmarks,
+                                      connections=mp_face_mesh.FACEMESH_TESSELATION, landmark_drawing_spec=None,
+                                      connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style())
+            mp_drawing.draw_landmarks(image=annotated_image,
+                                      landmark_list=face_landmarks,
+                                      connections=mp_face_mesh.FACEMESH_CONTOURS, landmark_drawing_spec=None,
+                                      connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style())
+            mp_drawing.draw_landmarks(image=annotated_image,
+                                      landmark_list=face_landmarks,
+                                      connections=mp_face_mesh.FACEMESH_IRISES, landmark_drawing_spec=None,
+                                      connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_iris_connections_style())
+            cv.imshow('Video', annotated_image)
 
 
 def faceFeaturesDetection(image, model):
@@ -42,7 +81,7 @@ def faceFeaturesDetection(image, model):
 
 def collectData():
     actions = np.array(['left', 'right', 'straight'])
-    DATA_PATH = os.path.join('Extracted_Values')
+    DATA_PATH = os.path.join('Extracted_Values3')
 
     for action in actions:
         for sequence in os.listdir('Dataset2/' + action):
@@ -83,5 +122,95 @@ def collectData():
 
                     if cv.waitKey(10) == ord('q'):
                         break
+        cap.release()
+        cv.destroyAllWindows()
+
+
+def collectData2():
+    actions = np.array(['lchange', 'rchange', 'straight'])
+    DATA_PATH = os.path.join('Extracted_Values_Sorted')
+
+    for action in actions:
+        for sequence in os.listdir('Dataset_Sorted/' + action):
+            try:
+                os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+            except:
+                pass
+
+    mp_holistic = mp.solutions.holistic  # Holistic model
+    mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
+    sequenceLength = 150
+
+    with mp_holistic.Holistic(min_detection_confidence=0.6, min_tracking_confidence=0.6) as holistic:
+        for action in actions:
+            for file in os.listdir('Dataset_Sorted/' + action):
+                cap = cv.VideoCapture('Dataset_Sorted/' + action + "/" + file)
+                for frameNumber in range(sequenceLength):
+
+                    ret, frame = cap.read()
+                    if not ret:  # Error handling
+                        print("Can't receive frame!")
+                        break
+
+                    frame = cv.resize(frame, None, fx=0.5, fy=0.5,
+                                      interpolation=cv.INTER_LINEAR)  # Resize the image for better performance
+                    image, results = faceFeaturesDetection(frame, holistic)
+
+                    keypoints = extractKeypoints(results)
+                    npyPath = os.path.join(DATA_PATH, action, str(file), str(frameNumber))
+                    np.save(npyPath, keypoints)
+
+                    cv.imshow('Video', image)
+
+                    if cv.waitKey(10) == ord('q'):
+                        break
+        cap.release()
+        cv.destroyAllWindows()
+
+
+def collectDataWithIris():
+    actions = np.array(['lchange', 'rchange', 'straight'])
+    DATA_PATH = os.path.join('Extracted_Values_Sorted_Iris_05_12_12_59')
+
+
+    for action in actions:
+        for sequence in os.listdir('Dataset_Sorted/' + action):
+            try:
+                os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+            except:
+                pass
+
+    mp_face_mesh = mp.solutions.face_mesh  # FaceMesh model
+    sequenceLength = 150
+
+    with mp_face_mesh.FaceMesh(min_detection_confidence=0.6, min_tracking_confidence=0.6, max_num_faces=1,
+                               refine_landmarks=True) as face_mesh:
+        for action in actions:
+            for file in os.listdir('Dataset_Sorted/' + action):
+                cap = cv.VideoCapture('Dataset_Sorted/' + action + "/" + file)
+
+                for frameNumber in range(sequenceLength):
+                    ret, frame = cap.read()
+                    if not ret:  # Error handling
+                        print("Can't receive frame!")
+                        break
+                    frame = cv.resize(frame, None, fx=0.5, fy=0.5,
+                                      interpolation=cv.INTER_LINEAR)  # Resize the image for better performance
+
+                    image, results = faceFeaturesDetection(frame, face_mesh)
+                    drawCustomLandmarksWithIris(image, results, mp_face_mesh)
+
+                    keypoints = extractKeypointsWithIris(results)
+                    npyPath = os.path.join(DATA_PATH, action, str(file), str(frameNumber))
+                    np.save(npyPath, keypoints)
+
+                    print("Processing frame number:", frameNumber)
+
+                    # Very important knowledge!!!
+                    # https://stackoverflow.com/questions/67141844/how-do-i-get-the-coordinates-of-face-mash-landmarks-in-mediapipe kurwa kozak
+
+                    if cv.waitKey(10) == ord('q'):
+                        break
+
         cap.release()
         cv.destroyAllWindows()
--- DriverBehaviorAnalyser/main.py
+++ DriverBehaviorAnalyser/main.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 import os
 import mediapipe as mp
+import shutil
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
@@ -11,6 +12,7 @@ from sklearn.metrics import multilabel_confusion_matrix, accuracy_score
 
 import DriverAnalyser
 import Model
+import DriverPrediction
 
 
 def preprocessData():
@@ -66,11 +68,50 @@ def probabilityBar(res, actions, frame):
     out_frame = frame.copy()
     for num, prob in enumerate(res):
         cv.rectangle(out_frame, (0, 60 + num * 40), (int(prob * 100), 90 + num * 40), colors[num], -1)
-        cv.putText(out_frame,actions[num], (0, 85 + num * 40), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv.LINE_AA)
+        cv.putText(out_frame, actions[num], (0, 85 + num * 40), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
+                   cv.LINE_AA)
     return out_frame
 
 
-def makePredictions():
+def makePredictionOnWebcam():
+    # TODO: Poprawić implementacje Real-Time. Caly czs wyrzuca rezultat że jade w lewo nie jadąc w lewo
+
+    sequence = []
+    actions = np.array(['left', 'right', 'straight'])
+    mp_holistic = mp.solutions.holistic
+
+    model = Model.createModel('Adamax', actions)  # New LSTM model
+    model.load_weights('action.h5')  # Load weights into the network
+
+    cap = cv.VideoCapture(0)  # Capture the video from a webcam
+
+    with mp_holistic.Holistic(min_detection_confidence=0.6, min_tracking_confidence=0.6) as holistic:
+        while cap.isOpened():
+            ret, frame = cap.read()
+
+            image, results = DriverAnalyser.faceFeaturesDetection(frame, holistic)  # Detect face landmarks of a driver
+            DriverAnalyser.drawCustomLandmarks(image, results, mp_holistic)  # Customize the landmarks
+
+            # Prediction logic
+            keypoints = DriverAnalyser.extractKeypoints(results)    # Extract the keypoints of the face
+            sequence.insert(0, keypoints)
+            sequence = sequence[:80]   # Load last 150 values
+
+            if len(sequence) == 80:
+                res = model.predict(np.expand_dims(sequence, axis=0))[0]
+                print(actions[np.argmax(res)])
+
+            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            image = probabilityBar(res, actions, image)
+
+            cv.imshow('Image', image)
+            if cv.waitKey(10) & 0xFF == ord('q'):
+                break
+    cap.release()
+    cv.destroyAllWindows()
+
+
+def makePredictions(testVideo):
     sequence = []
     actions = np.array(['left', 'right', 'straight'])
 
@@ -79,7 +120,7 @@ def makePredictions():
     model = Model.createModel('Adamax', actions)  # New LSTM model
     model.load_weights('action.h5')  # Load weights into the network
 
-    cap = cv.VideoCapture('test_vid3.avi')
+    cap = cv.VideoCapture(testVideo)
 
     with mp_holistic.Holistic(min_detection_confidence=0.6, min_tracking_confidence=0.6) as holistic:
         while cap.isOpened():
@@ -91,14 +132,14 @@ def makePredictions():
                 break
 
             image, results = DriverAnalyser.faceFeaturesDetection(frame, holistic)
-            DriverAnalyser.drawCustomLandmarks(image, results, mp_holistic)
+            #DriverAnalyser.drawCustomLandmarks(image, results, mp_holistic)
 
             # Prediction logic
             keypoints = DriverAnalyser.extractKeypoints(results)
             sequence.append(keypoints)
-            sequence = sequence[:150]
+            sequence = sequence[:80]
 
-            if len(sequence) == 150:
+            if len(sequence) == 80:
                 #res = model.predict(np.expand_dims(sequence, axis=0))[0]
                 print(actions[np.argmax(res)])
 
@@ -111,5 +152,36 @@ def makePredictions():
     cv.destroyAllWindows()
 
 
+def extractAvi():
+    """
+    Extraction algorithm which extracts .avi videos from the raw dataset
+    """
+    actions = np.array(['rturn', 'rchange', 'lturn', 'lchange', 'straight'])
+    os.chdir('Full_Dataset')
+    for action in actions:
+        dest = '/Users/kacpe/OneDrive/Pulpit/DBA/DriverBehaviorAnalyser/Extracted_Videos_Avi2/' + str(action)
+        os.chdir(action)
+        for folder in os.listdir():
+            os.chdir(folder)
+            for file in os.listdir():
+                if file == 'Extracted_Videos_Avi':
+                    pass
+                else:
+                    shutil.copy(file, dest)
+            os.chdir('..')
+        os.chdir('..')
+
+
 if __name__ == '__main__':
-    makePredictions()
+    # TODO: Zoptymalizować model
+    """
+    Udało sie wyciagnac landmarki kazdej z wsporzednych (x,y,z) i zapisac je do tablicy numpy jak w poprzednim rozwiazaniu. Teraz trzeba 
+    puscic caly dataset przez algorytm i te wartosci wrzucic do modelu. 
+    """
+
+
+    #makePredictions('test_vid_left.avi')
+    DriverAnalyser.collectDataWithIris()
+    print("DONE!")
+    #DriverAnalyser.collectData2()
+    # DriverPrediction.preprocessData2()
